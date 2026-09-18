0b9a507eedb16859bf753181c3de9576c15ba079
kuldeeplovanshi <kulovanshi@bestpeers.com>
2026-09-18 16:51:46 +0530

    mount active storage for production

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
     end
   end
 
+  mount ActiveStorage::Engine => "/rails/active_storage"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
