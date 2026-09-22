78714a4f59d0f8c920dd65a75d7ff2deec66f655
kuldeeplovanshi <kulovanshi@bestpeers.com>
2026-09-22 14:13:40 +0530

    Remove enum from menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ApplicationRecord
   has_many :menu_categories, dependent: :destroy
 
-  enum :category_type, { our_menu: 0, specials: 1, drinks: 2 }
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class MenuTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is invalid without a name" do
+    menu = Menu.new(name: "")
+
+    assert_not menu.valid?
+    assert_includes menu.errors[:name], "can't be blank"
+  end
+
+  test "is invalid with a duplicate name" do
+    menu = Menu.new(name: menus(:one).name)
+
+    assert_not menu.valid?
+    assert_includes menu.errors[:name], "has already been taken"
+  end
 end
